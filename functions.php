d9de7f6ebf9e020cead1716ec90530be03fe2686
coiy <coiy@coiy-macbook-pro.local>
2015-10-30 15:11:29 +0900

    preparing for responsive

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 function lion_load_style() {
   wp_enqueue_style('style-lion', get_stylesheet_uri());
+  wp_enqueue_script('jquery1113', get_template_directory_uri() . '/js/jquery-1.11.3.min.js');
+  wp_enqueue_script('bxslider', get_template_directory_uri() . '/js/jquery.bxslider.min.js');
+  wp_enqueue_script('function', get_template_directory_uri() . '/js/function.js');
+  wp_enqueue_script('getdata', get_template_directory_uri() . '/js/getdata.js');
 }
 add_action('wp_enqueue_scripts', 'lion_load_style');
 
--- header.php
+++ header.php
@@ -6,6 +6,8 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href="<?php echo get_template_directory_uri() . '/css/common_sp.css'; ?>" rel="stylesheet" media="screen and (max-width: 1023px)" />
+  <link href="<?php echo get_template_directory_uri() . '/css/second_sp.css'; ?>" rel="stylesheet" media="screen and (max-width: 1023px)" />
 	<?php wp_head(); ?>
 	<style type="text/css">
 	@charset "UTF-8";
@@ -18,6 +20,6 @@
 <body>
   <div id="header_contents">
     <div class="header">
-      <p class="logo"><a href="http://lion.or.jp/testblog">風に立つライオン基金</a><span class="slogon">一人一人の小さな思いが、沢山の小さな生命を支えられることを信じます。</span></p>
+      <p class="logo"><a href="<?php echo esc_url(home_url('/'));?>">風に立つライオン基金</a><span class="slogon">一人一人の小さな思いが、沢山の小さな生命を支えられることを信じます。</span></p>
     </div>
   </div>
