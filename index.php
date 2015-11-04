9bbcc5364f58ad01dbe3401bf0a3797fba06ba93
Sung-nyoun Kim <tityrus@gmail.com>
2015-11-04 11:55:07 +0900

    sidebar edited.

--- index.php
+++ index.php
@@ -29,10 +29,10 @@
 				if(have_posts()) {
 					while(have_posts()) {
 							the_post();
-							_e("Category", 'lion');
-							the_category('&gt;', 'multiple');
+							
+
 							?>
-							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+<!--							<h3><a href="--><?php //the_permalink(); ?><!--">--><?php //the_title(); ?><!--</a></h3>-->
 									<?php the_post_thumbnail(small-thumbnail); ?>
 									<?php the_content(); ?>
 				<?php
--- sidebar.php
+++ sidebar.php
@@ -2,8 +2,9 @@
   <li class="list_about"><a href="http://lion.or.jp/testblog/about">風に立つライオン基金とは</a></li>
   <li class="list_orga"><a href="http://lion.or.jp/testblog/company">組織概要</a></li>
   <li class="list_purpose"><a href="http://lion.or.jp/testblog/purpose">事業目的</a></li>
-  <li class="list_contents"><a href="">活動内容</a></li>
+  <li class="list_contents"><a href="">活動</a></li>
   <li class="list_support"><a href="http://lion.or.jp/testblog/donation">基金へのご支援</a></li>
-  <li class="list_blog"><a href="">スタッフブログ</a></li>
-  <li class="list_community"><a href="">メンバー間コミュニティ</a></li>
+  <li class="list_blog"><a href="/comingsoon.html">メンバー募集</a></li>
+  <li class="list_community"><a href="http://lion.or.jp/testblog/">風の広場</a></li>
+  <li class="list_ouendan"><a href="/ouendan.html">風の応援団</a></li>
 </ul>
